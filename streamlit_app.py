2d09ecdfc9a0e7821e1e40ccb0a2347683b66943
Zhaoqi06 <liuzhaoqi06@icloud.com>
2025-12-17 19:48:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.set_page_config(page_title="首页",layout="wide")
 if "is_login" not in st.session_state or not st.session_state["is_login"]:
     st.error("请先登录！！！")
     # 尝试使用英文路径或相对路径
-    st.markdown('<meta http-equiv="refresh" content="1;URL=./login" />', unsafe_allow_html=True)
+    st.switch_page("pages/login.py")
 else:
    st.title(f"欢迎回来，{st.session_state['username']}")
