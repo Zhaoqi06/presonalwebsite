fb4c5bc4ad6ad8a83549a7a66b86179978be5ebb
Zhaoqi06 <liuzhaoqi06@icloud.com>
2025-12-21 13:58:03 +0800

    Update Study.py

--- pages/Study.py
+++ pages/Study.py
@@ -6,7 +6,7 @@ if "logged_in" not in st.session_state or not st.session_state.logged_in:
     st.switch_page("pages/login.py")
 
 # 使用 selectbox 实现导航
-nav = st.sidebar.selectbox("导航栏", ["首页","任务查看","提交入口", "高等数学","线性代数","英语四级", "英语六级","雅思","TED","专四","专升本", "计算机","Python","C语言","MATLAB","STM32","51单片机","MYSQL","数学竞赛","英语竞赛"，"论文"])
+nav = st.sidebar.selectbox("导航栏", ["首页","任务查看","提交入口", "高等数学","线性代数","英语四级", "英语六级","雅思","TED","专四","专升本", "计算机","Python","C语言","MATLAB","STM32","51单片机","MYSQL","数学竞赛","英语竞赛","论文"])
 if nav == "首页":
     st.markdown("<style>.stApp{background:linear-gradient(123deg,#F1FAEE 0%,#A8DADC 100%);}</style>",unsafe_allow_html=True)
     st.title("欢迎来到学习板块！")
