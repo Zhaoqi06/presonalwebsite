79b826ff29df6ac80f119de79ec63d0bbb678e1f
Zhaoqi06 <liuzhaoqi06@icloud.com>
2025-12-26 10:13:33 +0800

    Update association.py

--- pages/association.py
+++ pages/association.py
@@ -42,11 +42,11 @@ elif nav == "协会成员":
         id = []
         identity = []
         for i in range(length):
-            name.append(file_data.iloc[i, 6])
-            id.append(str(file_data.iloc[i, 5]))
-            if file_data.iloc[i, 9] != "非国际交流协会成员":
+            if file_data.iloc[i, 9] == "非国际交流协会成员":
                 continue
             else:
+                name.append(file_data.iloc[i, 6])
+                id.append(str(file_data.iloc[i, 5]))
                 identity.append(file_data.iloc[i, 9])
         Mark = file_data.iloc[:, 6]
         def Name():
